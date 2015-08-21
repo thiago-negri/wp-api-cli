52a83cdfeb0b7ed9967bcbdab01a5638ac3f0453
Thiago B. Negri <evohunz@gmail.com>
2015-08-21 20:26:49 -0300

    Add post create command

--- wp-api-cli.js
+++ wp-api-cli.js
@@ -2,17 +2,25 @@
 
 /* FIXME Change 'context' to an object (prototype). */
 
-var cli     = require( 'cli'        ),
+var	fs      = require( 'fs'         ),
+	cli     = require( 'cli'        ),
 	request = require( 'request'    ),
 	oauth   = require( 'oauth-lite' );
 
-cli.setUsage( 'wp-api-cli [OPTIONS]' );
+cli.setUsage( 'wp-api-cli [OPTIONS] post [list|create]' );
 
 cli.parse({
-	url:   [ 'u',   '(Required) Set base URL to use',                                'STRING' ],
-	user:  [ false, 'Set username to use for HTTP Basic Authentication',             'STRING' ],
-	pass:  [ false, 'Set password to use for HTTP Basic Authentication',             'STRING' ],
-	debug: [ 'd',   'Turns on debugging mode, will output interactions with server' ]
+	url:   [ 'u', '(Required) Set base URL to use', 'STRING' ],
+	debug: [ 'd', 'Turns on debugging mode, will output interactions with server' ],
+
+	/* HTTP Basic-Auth */
+	user: [ false, 'Set username to use for HTTP Basic Authentication', 'STRING' ],
+	pass: [ false, 'Set password to use for HTTP Basic Authentication', 'STRING' ],
+
+	/* Post */
+	post_type:    [ false, 'Post type to be created or updated', 'STRING', 'post' ],
+	post_title:   [ false, 'Post title to be created or updated', 'STRING', '' ],
+	post_content: [ false, 'Content of post to be created or updated', 'STRING' ]
 });
 
 cli.main( function ( args, options ) {
@@ -118,7 +126,7 @@ function step3_authenticate( context ) {
 		auth: {
 			user: context.options.user,
 			pass: context.options.pass,
-			sendImmediately: false
+			sendImmediately: true
 		}
 	});
 
@@ -135,6 +143,8 @@ function step4_handleCommand( context ) {
 
 	if ( command === 'post list' ) {
 		step5_listPosts( context );
+	} else if ( command === 'post create' ) {
+		step5_createPost( context );
 	}
 }
 
@@ -146,6 +156,44 @@ function step5_listPosts( context ) {
 	}
 
 	context.authenticatedRequest.get( url, function ( error, response, body ) {
+		console.log( body );
+	});
+}
+
+function step5_createPost( context ) {
+	var fileName = context.args[2];
+
+	if ( fileName ) {
+		if ( fileName === '-' ) {
+			cli.withStdin( function ( stdin ) {
+				context.postContent = stdin;
+				step5_doCreatePost( context );
+			});
+		} else {
+			fs.readFile( fileName, 'utf8', function ( error, data ) {
+				if ( error ) {
+					cli.fatal( 'Error while loading post content from file ' + fileName + ': ' + error );
+				}
+				context.postContent = data;
+				step5_doCreatePost( context );
+			});
+		}
+	} else {
+		context.postContent = context.options.post_content;
+		step5_doCreatePost( context );
+	}
+}
+
+function step5_doCreatePost( context ) {
+	var config = {
+			url: context.wpApiUrl + 'wp/v2/posts',
+			body: {
+				type: context.options.post_type,
+				title: context.options.post_title,
+				content: context.postContent
+			}
+		};
+	context.authenticatedRequest.post( config, function ( error, response, body ) {
 		console.log( body );
 	});
 }
