92d7b60edc2cf18e243578bbf39cd089e325b0a3
Thiago B. Negri <evohunz@gmail.com>
2015-08-22 13:17:58 -0300

    OAuth working and 'post_create' command

--- README.md
+++ README.md
@@ -8,19 +8,30 @@ Installing
 
 1. WP-API v2 depends on the latest WordPress.org, so [download it from Subversion](https://wordpress.org/download/svn/).
 2. Install [WP-API plugin from Git](https://github.com/WP-API/WP-API) and activate it.
-3. Install [WP-API/OAuth1 from Git](https://github.com/WP-API/OAuth1) to support OAuth authentication, activate it.
+3. Install [Node.js](https://nodejs.org/).
+4. Download this repository, and install it on your system: `npm install`
+
+### Authenticating with OAuth
+
+1. Install [WP-API/OAuth1 from Git](https://github.com/WP-API/OAuth1) to support OAuth authentication, activate it.
     - If your WordPress install is inside a folder, please check [pull request 78](https://github.com/WP-API/OAuth1/pull/78).
-4. Install [WP-API/Basic-Auth from Git](https://github.com/WP-API/Basic-Auth) to support HTTP Basic Authentication, activate it.
-5. Install [WP-CLI](http://wp-cli.org/) to generate OAuth keys.
-6. Create a new OAuth key for the CLI: `wp oauth1 add`.
-7. Install [Node.js](https://nodejs.org/).
-8. Download this repository, and install it on your system: `npm install`.
-9. etc -- OAuth authentication not working so far.
+2. Install [WP-CLI](http://wp-cli.org/) to generate OAuth keys.
+3. Go to your WordPress installation folder and create a new consumer key and secret for this CLI: `wp oauth1 add`
+4. Authenticate this CLI: `wp-api-cli --site=http://example.com --oauth_key=CONSUMER_KEY --oauth_secret=CONSUMER_SECRET authenticate`
+5. It will ask for an authorization token. Follow the steps on the browser, copy the authorization token and paste it in the console.
+6. It will create a file with the OAuth tokens (`oauth.json`) for further use -- this is a sensitive file, make sure to protected it!
+
+### Authenticating with HTTP Basic Auth
+
+1. Install [WP-API/Basic-Auth from Git](https://github.com/WP-API/Basic-Auth) to support HTTP Basic Authentication, activate it.
+2. Use options `--user` and `--pass` with every request you make.
 
 Examples
 --------
 
-List all posts: `wp-api-cli --url=http://example.com --user=foo --pass=bar post list`
+List all posts: `wp-api-cli --site=http://example.com post_list`
+
+Create a new post: `wp-api-cli --site=http://example.com --post_title="Hello, WP-API" --post_content="WP-API is awesome!" post_create`
 
 Useful Links
 ------------
@@ -28,3 +39,7 @@ Useful Links
 - [WordPress.org Subversion download](https://wordpress.org/download/svn/)
 - [WP-API project on GitHub](https://github.com/WP-API/WP-API)
 - [Online documentation for WP-API v2](http://v2.wp-api.org/)
+- [WP-API/OAuth1 project on GitHub](https://github.com/WP-API/OAuth1)
+- [WP-API/Basic-Auth project on GitHub](https://github.com/WP-API/Basic-Auth)
+- [Node.js](https://nodejs.org/)
+- [WP-CLI](http://wp-cli.org/)
--- wp-api-cli.js
+++ wp-api-cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
-var	fs    = require( 'fs'           ),
-	cli   = require( 'cli'          ),
-	WpApi = require( './lib/wp-api' );
+var	fs       = require( 'fs'           ),
+	cli      = require( 'cli'          ),
+	readline = require( 'readline'     ),
+	open     = require( 'open'         ),
+	WpApi    = require( './lib/wp-api' );
 
 cli.setUsage( 'wp-api-cli [OPTIONS] <COMMAND>' );
 
@@ -19,49 +21,66 @@ cli.parse({
 	user: [ 'u', 'Set username to use for HTTP Basic Authentication', 'STRING' ],
 	pass: [ 'p', 'Set password to use for HTTP Basic Authentication', 'STRING' ],
 
+	/* OAuth */
+	oauth_key:    [ false, 'OAuth Consumer Key', 'STRING' ],
+	oauth_secret: [ false, 'OAuth Consumer Secret', 'STRING' ],
+	oauth_file:   [ 'o', 'OAuth authorization file created by "authenticate" command', 'FILE', 'oauth.json' ],
+
 	/* Post */
 	post_type:    [ false, 'Post type to be created or updated', 'STRING', 'post' ],
-	post_title:   [ false, 'Post title to be created or updated', 'STRING', '' ],
+	post_title:   [ false, 'Post title to be created or updated', 'STRING' ],
 	post_content: [ false, 'Content of post to be created or updated', 'STRING' ],
 	post_file:    [ false, 'Content of FILE will be used as content of post', 'FILE' ]
 }, {
-	post_list:   'List all published posts',
-	post_create: 'Creates a new post'
+	authenticate: 'Authenticate with site, will issue OAuth tokens',
+	post_list:    'List all published posts',
+	post_create:  'Creates a new post'
 });
 
 cli.main( function ( args, options ) {
 	var	config,
 		wpApi;
 
+	validateAndSanitize( options );
+
 	/* Allow connections to SSL sites without certs */
 	if ( options.insecure ) {
 		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 	}
 
-	validateAndSanitize( options );
+	if ( ( ! options.user || ! options.pass ) && cli.command !== 'authenticate' ) {
+		cli.info( 'Using OAuth authentication.' );
+		fs.readFile( options.oauth_file, 'utf8', function ( error, content ) {
+			var oauthConfig;
 
-	config = {
-		site:  options.site,
-		user:  options.user,
-		pass:  options.pass,
-		debug: options.debug
-	};
+			if ( error ) {
+				cli.fatal( error );
+			}
 
-	wpApi = new WpApi( config );
+			oauthConfig = JSON.parse( content );
 
-	switch ( cli.command ) {
-		case 'post_list':
-			wpApi.listPosts( function ( error, data ) {
-				if ( error ) {
-					cli.fatal( error );
-				}
-				console.log( data );
-			});
-			break;
+			config = {
+				site:  options.site,
+				debug: options.debug,
+				oauth: oauthConfig
+			};
 
-		case 'post_create':
-			createPost( args, options );
-			break;
+			wpApi = new WpApi( config );
+
+			processCommand( args, options, wpApi );
+		});
+	} else {
+		cli.info( 'Using HTTP Basic authentication.' );
+		config = {
+			site:  options.site,
+			user:  options.user,
+			pass:  options.pass,
+			debug: options.debug
+		};
+
+		wpApi = new WpApi( config );
+
+		processCommand( args, options, wpApi );
 	}
 });
 
@@ -72,21 +91,102 @@ function validateAndSanitize( options ) {
 	if ( ! options.site ) {
 		cli.fatal( 'Missing base URL. Please, set a base URL by using `-s` or `--site`.' );
 	}
-
-	if ( ! options.user || ! options.pass ) {
-		cli.fatal( 'Please, set a username (`-u`) and password (`-p`) to use for HTTP Basic Authentication.' );
-	}
-
 	if ( options.site[-1] !== '/' ) {
 		options.site = options.site + '/';
 	}
 }
 
-function createPost( args, options ) {
+function processCommand( args, options, wpApi ) {
+	switch ( cli.command ) {
+		case 'authenticate':
+			authenticate( args, options, wpApi );
+			break;
+
+		case 'post_list':
+			wpApi.listPosts( function ( error, data ) {
+				if ( error ) {
+					cli.fatal( error );
+				}
+				console.log( data );
+			});
+			break;
+
+		case 'post_create':
+			createPost( args, options, wpApi );
+			break;
+	}
+}
+
+function authenticate( args, options, wpApi ) {
+	var oauthConfig = {
+		oauth_consumer_key:    options.oauth_key,
+		oauth_consumer_secret: options.oauth_secret
+	};
+
+	if ( ! oauthConfig.oauth_consumer_key || ! oauthConfig.oauth_consumer_secret ) {
+		cli.fatal( 'Missing OAuth consumer key and secret.' );
+	}
+
+	wpApi.fetchOauthRequestToken( oauthConfig, function ( error, response ) {
+		var rl;
+		if ( error ) {
+			cli.fatal( error );
+		}
+
+		oauthConfig.oauth_token = response.oauth_token;
+		oauthConfig.oauth_token_secret = response.oauth_token_secret;
+
+		cli.info( 'Follow authorization process on browser.' );
+		open( response.authorizeUrl );
+
+		rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+
+		rl.question( 'Enter your verification token: ', function ( verificationToken ) {
+			if ( ! verificationToken ) {
+				return;
+			}
+
+			oauthConfig.oauth_verifier = verificationToken;
+
+			wpApi.fetchOauthAccessToken( oauthConfig, function ( error, response ) {
+				if ( error ) {
+					cli.fatal( error );
+				}
+
+				var oauthCredentials = {
+					oauth_consumer_key:    oauthConfig.oauth_consumer_key,
+					oauth_consumer_secret: oauthConfig.oauth_consumer_secret,
+					oauth_token:           response.oauth_token,
+					oauth_token_secret:    response.oauth_token_secret
+				};
+
+				fs.writeFile( 'oauth.json', JSON.stringify( oauthCredentials ), function ( error ) {
+					if ( error ) {
+						cli.fatal( error );
+					}
+					cli.ok( 'Credentials saved as "oauth.json". This is a sensitive file, make sure to protect it.');
+					rl.close();
+				});
+			});
+		});
+	});
+}
+
+function createPost( args, options, wpApi ) {
 	var fileName = options.post_file,
 		thePost = {
 			type:  options.post_type,
 			title: options.post_title
+		},
+		callback = function ( error, createdPost ) {
+			if ( error ) {
+				cli.fatal( error );
+			}
+			cli.ok( 'Post created.' );
+			console.log( createdPost );
 		};
 
 	if ( fileName ) {
