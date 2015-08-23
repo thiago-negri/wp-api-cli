4fa6c44f5cf16c099c1541e7b8c747eeb9769531
Thiago B. Negri <evohunz@gmail.com>
2015-08-23 11:28:27 -0300

    Extract Insecure module

--- wp-api-cli.js
+++ wp-api-cli.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
 
-var	cli      = require( 'cli'                 ),
-	WpApi    = require( './lib/wp-api'        ),
-	cliAuth  = require( './lib/modules/auth'  ),
-	cliPosts = require( './lib/modules/posts' ),
-	cliPages = require( './lib/modules/pages' ),
-	cliMedia = require( './lib/modules/media' ),
+var	cli         = require( 'cli'                     ),
+	WpApi       = require( './lib/wp-api'            ),
+	cliInsecure = require( './lib/modules/insecure'  ),
+	cliAuth     = require( './lib/modules/auth'      ),
+	cliPosts    = require( './lib/modules/posts'     ),
+	cliPages    = require( './lib/modules/pages'     ),
+	cliMedia    = require( './lib/modules/media'     ),
 
-	modules  = loadModules(),
-	options  = buildOptions(),
-	commands = buildCommands();
+	modules     = loadModules(),
+	options     = buildOptions(),
+	commands    = buildCommands();
 
 function loadModules() {
 	var	modules;
 	modules = [
+		cliInsecure,
 		cliAuth,
 		cliPosts,
 		cliPages,
@@ -26,17 +28,16 @@ function buildOptions() {
 	var	options = {
 			site:  [ 's', '(Required) Set base URL to use', 'STRING' ],
 			debug: [ 'd', 'Turns on debugging mode, will output interactions with server' ],
-
-			/* Support HTTPS with self signed certificate. */
-			insecure: [ 'k', 'Allow connections to SSL sites without certs' ],
 		};
 
 	/* Load options from modules. */
 	modules.forEach( function ( mod ) {
 		var key;
-		for ( key in mod.options ) {
-			if ( mod.options.hasOwnProperty( key ) ) {
-				options[ key ] = mod.options[ key ];
+		if ( mod.options ) {
+			for ( key in mod.options ) {
+				if ( mod.options.hasOwnProperty( key ) ) {
+					options[ key ] = mod.options[ key ];
+				}
 			}
 		}
 	});
@@ -50,9 +51,11 @@ function buildCommands() {
 	/* Load commands from modules. */
 	modules.forEach( function ( mod ) {
 		var key;
-		for ( key in mod.commands ) {
-			if ( mod.commands.hasOwnProperty( key ) ) {
-				commands[ key ] = mod.commands[ key ].label;
+		if ( mod.commands ) {
+			for ( key in mod.commands ) {
+				if ( mod.commands.hasOwnProperty( key ) ) {
+					commands[ key ] = mod.commands[ key ].label;
+				}
 			}
 		}
 	});
@@ -72,11 +75,6 @@ cli.main( function ( args, options ) {
 
 	validateAndSanitize( options );
 
-	/* Allow connections to SSL sites without certs */
-	if ( options.insecure ) {
-		process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-	}
-
 	config = {
 		site:  options.site,
 		debug: options.debug,
@@ -95,28 +93,26 @@ cli.main( function ( args, options ) {
  */
 function initModules( cli, args, options, api, callback ) {
 	var	go = function ( i ) {
-			return function ( cli, args, options, api, cb ) {
-				if ( i >= modules.length ) { /* Done initializing all modules, fire the final callback. */
-					cb();
-				} else { /* Still have modules to initialize. */
-					if ( modules[i].init ) { /* The current module supports initialization? */
-						/* Initialize the module, the callback will handle errors and advance our module-initialization-index. */
-						modules[i].init( cli, args, options, api, function ( error ) {
-							if ( error ) {
-								cli.fatal( error );
-							}
-							/* Module sucessfuly initialized, go to next module to initialize. */
-							go( i + 1 )( cli, args, options, api, callback );
-						});
-					} else {
-						/* The module does not need to be initialized, go to next module to initialize. */
-						go( i + 1 )( cli, args, options, api, callback );
-					}
+			if ( i >= modules.length ) { /* Done initializing all modules, fire the final callback. */
+				callback();
+			} else { /* Still have modules to initialize. */
+				if ( modules[i].init ) { /* The current module supports initialization? */
+					/* Initialize the module, the callback will handle errors and advance our module-initialization-index. */
+					modules[i].init( cli, args, options, api, function ( error ) {
+						if ( error ) {
+							cli.fatal( error );
+						}
+						/* Module sucessfuly initialized, go to next module to initialize. */
+						go( i + 1 );
+					});
+				} else {
+					/* The module does not need to be initialized, go to next module to initialize. */
+					go( i + 1 );
 				}
-			};
+			}
 		};
 	/* Start module initialization from the very first one. */
-	go( 0 )( cli, args, options, api, callback );
+	go( 0 );
 }
 
 /**
@@ -135,11 +131,13 @@ function processCommand( args, options, wpApi ) {
 	var	i, arr, len, mod, key;
 	for (i = 0, arr = modules, len = arr.length; i < len; i += 1) {
 		mod = arr[i];
-		for ( key in mod.commands ) {
-			if ( mod.commands.hasOwnProperty( key ) ) {
-				if ( cli.command === key ) {
-					mod.commands[ key ].handler( cli, args, options, wpApi );
-					return;
+		if ( mod.commands ) {
+			for ( key in mod.commands ) {
+				if ( mod.commands.hasOwnProperty( key ) ) {
+					if ( cli.command === key ) {
+						mod.commands[ key ].handler( cli, args, options, wpApi );
+						return;
+					}
 				}
 			}
 		}
