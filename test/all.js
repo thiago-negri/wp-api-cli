3719695d1f309ca2e5377f39c39ec98e99bf9334
Thiago B. Negri <evohunz@gmail.com>
2015-09-13 11:28:27 -0300

    Disable log messages on tests

--- test/all.js
+++ test/all.js
@@ -3,6 +3,11 @@
 var	nodeunit = require( 'nodeunit' ),
 	reporter = nodeunit.reporters[ 'default' ];
 
+/**
+ * Disable logging.
+ */
+require( '../lib/utils/log' ).enabled = false;
+
 process.chdir( __dirname );
 reporter.run([
 	'test-auth.js',
